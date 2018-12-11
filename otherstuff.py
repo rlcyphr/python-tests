043bdbe192ce1ea7cc435eb39e702dc777abb851
Connor <connor.fallon@centralfax.co.uk>
2018-12-11 22:06:36 +0000

    Backup

---
+++ otherstuff.py
@@ -0,0 +1,41 @@
+# making classes in python 2.7
+
+# RPG game
+
+class Creature(object):
+
+    # shall be extended with different types of class (e.g. aviary, ground-based, water, hostile, etc. )
+    
+    def __init__(self, name, type, speed, damage, items):
+        self.name = name
+        self.type = creatureType
+        self.speed = speed
+        self.damage = damage
+        self.items = items # their inventory - will be an array containing Item objects
+
+
+class Item(object):
+    def __init__(itemType, owner, name):
+
+        # create the attributes of the item
+        # contains all of the information for what the item is, including the name, description, etc.
+        # abstract class extended for weapons, food, medicine, and all other types of item
+
+        self.type = itemType
+        self.owner = owner
+        self.name = name
+
+
+class Player(Creature):
+
+    # a particular type of creature
+    # the players that are part of the party
+
+    def __init__(self, cash, role):
+        self.name = name
+        self.cash = 0
+        self.role = role
+
+    
+
+
---
+++ stuff.py
@@ -0,0 +1,18 @@
+# tests for stuff
+
+import tkinter as tk
+
+class App(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.createButton()
+
+    def createButton(self):
+        newButton = tk.Button(self, text="This is a button.")
+        newButton.pack()
+
+root = tk.Tk()
+
+newthing = App(root)
