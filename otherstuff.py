85b01106e8e480b4967a1c076d4d6e3a19554cd7
rlcyphr <connor.fallon@centralfax.co.uk>
2018-12-21 22:36:44 +0000

    Begin to creature weapon class.

--- otherstuff.py
+++ otherstuff.py
@@ -32,19 +32,24 @@ class Creature:
 
 class Item:
 
-    def __init__(self, itemType, owner, name):
+    def __init__(self, itemType, name, owner=None):
 
         # create the attributes of the item
         # contains all of the information for what the item is, including the name, description, etc.
         # abstract class extended for weapons, food, medicine, and all other types of item
 
         self.type = itemType
-        self.owner = owner
         self.name = name
+        self.owner = owner
+        
 
 
-
+# -------------------------------
 # ---- Other classes go here ----
+# -------------------------------
+
+
+# ---- Creatures of all kinds ----
 
 
 class Player(Creature):
@@ -54,17 +59,13 @@ class Player(Creature):
 
     # 'role' is the specific feature of the player object
 
-    
-
-
-
     def __init__(self, speed=5, damage=10):
 
         
         
-        super().__init__('', 'Player', speed, damage, [], (0, 0))
-        self._roles = {'1': 'Knight'}
-        self.createPlayer()
+        super().__init__('', 'Player', speed, damage, [], (0, 0)) # create a creature object
+        self._roles = {'1': 'Knight', '2': 'Wizard', '3': 'Explorer'} 
+        self.createPlayer() # create the player part of the object
 
 
     def createPlayer(self):
@@ -97,14 +98,34 @@ class Player(Creature):
         return self._roles[str(role)]
 
 
-
-
 class Monster(Creature):
     # thing that is hostile to the player
     
 
     def __init__(self, name, damage, speed=5):
         super().__init__(name, 'Monster', speed, damage, [], (0, 0)) # make a Creature class
+
+
+# ---- Items ----
+
+class Weapon(Item):
+
+    # any kind of item that can be used to inflict damage on another thing
+
+    _weapons = [] # list of all the items of this type
+
+    def __init__(self, weapon_type, damage=None):
+
+        super().__init__('Weapon', weapon_type, damage)
+        Weapon._weapons.append(self)
+
+    @classmethod
+
+    def getList(self):
+        
+        for i in Weapon._weapons:
+            print(i.__dict__)
+
 
 
 
@@ -115,10 +136,23 @@ def main():
 
     # creation of a player
 
-    newPlayer = Player()
+    newPlayer = Player(15, 100)
 
     print("The role type of %s is %s. " % (newPlayer.name, newPlayer.role))
+    print("The speed is {s} and the damage is: {f}".format(s=newPlayer.speed, f=newPlayer.damage))
 
+    # creation of a monster
+
+    newMonster = Monster('Zombie', 10)
+
+    for i in newMonster.__dict__:
+        print(newMonster.__dict__[i])
+
+    newWeapon = Weapon('Sword', 10) # create a new weapon object
+
+    newWeapon2 = Weapon('Gun', 50) # other weapon in _weapons
+
+    Weapon.getList()
 
     
 
